a129c07fb3315422b8b6709e8a93f2b2c9705d10
Sudarshen <sudarshenk@gmail.com>
2026-05-26 18:17:15 +0200

    lesson: Images with python 1, learning how to proces imgages with python

--- Python/Scripting-and-Automation/ZTM-Python-Scripting/01-image-processing.py
+++ Python/Scripting-and-Automation/ZTM-Python-Scripting/01-image-processing.py
@@ -1,3 +1,33 @@
-from PIL import Image
+###############################################
 
-im = Image.open("")
+# Lesson: Images With Python
+
+from PIL import Image, ImageFilter
+
+# #  asiging a filter to the image
+# img = Image.open('./photos/pikachu.jpg')
+# filtered_img = img.filter(ImageFilter.BLUR)
+# filtered_img.save("blur.png", "png")
+
+# img = Image.open('./photos/pikachu.jpg')
+# filtered_img = img.filter(ImageFilter.SHARPEN)
+# filtered_img.save("SHARPEN.png", "png")
+
+# # converting image to grey
+# img = Image.open('./photos/pikachu.jpg')
+# filtered_img = img.convert('L')
+# filtered_img.save("grey.png", 'png')
+
+# img = Image.open('./photos/pikachu.jpg')
+# filtered_img = img.convert("P")
+# filtered_img.save("ok.png", 'png')
+
+
+
+
+
+
+###############################################
+
+# Lesson: Images With Python 2
+
