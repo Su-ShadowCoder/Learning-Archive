c794109a5455d22ef7480902451ed5a8c36bedc3
Sudarshen <sudarshenk@gmail.com>
2026-05-27 17:10:01 +0200

    second lesson about image processing

--- Python/Scripting-and-Automation/ZTM-Python-Scripting/01-image-processing.py
+++ Python/Scripting-and-Automation/ZTM-Python-Scripting/01-image-processing.py
@@ -13,10 +13,10 @@ from PIL import Image, ImageFilter
 # filtered_img = img.filter(ImageFilter.SHARPEN)
 # filtered_img.save("SHARPEN.png", "png")
 
-# # converting image to grey
-# img = Image.open('./photos/pikachu.jpg')
-# filtered_img = img.convert('L')
-# filtered_img.save("grey.png", 'png')
+# converting image to grey
+img = Image.open('./photos/pikachu.jpg')
+filtered_img = img.convert('L')
+filtered_img.save("grey2.png", 'png')
 
 # img = Image.open('./photos/pikachu.jpg')
 # filtered_img = img.convert("P")
@@ -29,5 +29,31 @@ from PIL import Image, ImageFilter
 
 ###############################################
 
-# Lesson: Images With Python 2
+# # Lesson: Images With Python 2
+
+
+# showing a image by using a third party aplication
+# it is necessary  to do that ina terminal because of the installation that has been used. as it is difficult to connect with other apps trough this method of installation. 
+# img = Image.open('./photos/pikachu.jpg')
+# filtered_img = img.convert('L')
+# crooked = filtered_img.rotate(90)
+# crooked.save("grey2.png", 'png')
+# crooked.show()
+
+
+# resize image
+# img = Image.open('./photos/pikachu.jpg')
+# filtered_img = img.convert('L')
+# resize = filtered_img.resize((300, 300))
+# resize.save("grey2.png", 'png')
+
+# cropping an image
+
+# img2 = Image.open('./photos/squirtle.jpg')
+# cropped_img = img2.crop((100, 100, 400, 400))
+# cropped_img.save("cropped_img.png","png")
+
+###############################################
+
+# # Lesson: Images With Python 3
 
