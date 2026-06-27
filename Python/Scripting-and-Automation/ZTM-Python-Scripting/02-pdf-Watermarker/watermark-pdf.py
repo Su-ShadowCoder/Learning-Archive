438632b7cc34d5fdaa2f457b7b8a70e90a75bafe
Sudarshen <sudarshenk@gmail.com>
2026-06-27 18:40:38 +0200

    made a pdf watermarker script that can watermark all pages in a pdf file. did rely heavily on document refrencing and general feedback from the gemini ai

---
+++ Python/Scripting-and-Automation/ZTM-Python-Scripting/02-pdf-Watermarker/watermark-pdf.py
@@ -0,0 +1,44 @@
+
+
+import os
+import argparse
+import pypdf
+import sys
+
+
+parser = argparse.ArgumentParser(description="A program that Watermarks all the pages of the pdf file")
+
+
+parser.add_argument("source_pdf", help="Input: pdf file that needs to be watermarked")
+parser.add_argument("destination_pdf", help="Output: Watermarked pdf file")
+parser.add_argument("watermark_pdf", help="pdf file with the watermark")
+
+
+args = parser.parse_args()
+
+input_path = args.source_pdf
+output_path = args.destination_pdf
+watermark = args.watermark_pdf
+
+
+if not os.path.exists(input_path):
+    print(f"Error: {input_path} not found.")
+    sys.exit(1)
+else:
+    print(f"Reading from:{input_path}")
+    print(f"Saving to:{output_path}")
+
+
+if not os.path.exists(watermark):
+    print(f"Error: {watermark} not found.")
+    sys.exit(1)
+else:
+    print(f"watermark from {watermark} found. processing!")
+
+stamp = pypdf.PdfReader(watermark).pages[0]
+writer = pypdf.PdfWriter(clone_from=input_path)
+for page in writer.pages:
+    page.merge_page(stamp, over=True)
+
+writer.write(output_path)
+writer.close()
