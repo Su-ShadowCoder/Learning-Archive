0d881318abec081cc0aad837463eabc2c160c560
Sudarshen <sudarshenk@gmail.com>
2026-06-29 14:35:41 +0200

    learned how send a email with python and how to apply the smtplib module doing that.

---
+++ Python/Scripting-and-Automation/ZTM-Python-Scripting/03-email-dynamic_content/email-dyn-cont.py
@@ -0,0 +1,19 @@
+# import smtplib
+# from email.message import EmailMessage
+
+
+# email = EmailMessage()
+# email['from'] = 'sudarshen kandasamy'
+# email['to'] = 'warlordwarlordwarlord007@gmail.com'
+# email['subject'] = "You won 5,000,000 euro!"
+
+
+# email.set_content('I am a Python Craft expert!,sending this from my IDE')
+
+
+# with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+#     smtp.ehlo()
+#     smtp.starttls()
+#     smtp.login('warlordwarlordwarlord007@gmail.com', 'nrtv iarr uwop zdwq')
+#     smtp.send_message(email)
+#     print('Job is done boss!!')
