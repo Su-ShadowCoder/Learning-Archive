fe360c85aaf21193d9c12eaaf82d44dd188569a7
Sudarshen <sudarshenk@gmail.com>
2026-07-01 01:40:36 +0200

    learned to use modules in order to make emails that has targeted content- depending on the database you could dynamicly adjust your content for said name with said what

--- Python/Scripting-and-Automation/ZTM-Python-Scripting/03-email-dynamic_content/email-dyn-cont.py
+++ Python/Scripting-and-Automation/ZTM-Python-Scripting/03-email-dynamic_content/email-dyn-cont.py
@@ -1,19 +1,28 @@
-# import smtplib
-# from email.message import EmailMessage
+import smtplib
+from email.message import EmailMessage
+#
+from string import Template
+from pathlib import Path 
+#
+
+#
+html = Template(Path('index.html').read_text())
+#
+
+email = EmailMessage()
+email['from'] = 'sudarshen kandasamy'
+email['to'] = 'warlordwarlordwarlord007@gmail.com'
+email['subject'] = "You won 5,000,000 euro!"
+
+#
+email.set_content(html.substitute(name= 'Tintin'), 'html')
+#
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('warlordwarlordwarlord007@gmail.com', 'nrtv iarr uwop zdwq')
+    smtp.send_message(email)
+    print('Job is done boss!!')
 
 
-# email = EmailMessage()
-# email['from'] = 'sudarshen kandasamy'
-# email['to'] = 'warlordwarlordwarlord007@gmail.com'
-# email['subject'] = "You won 5,000,000 euro!"
-
-
-# email.set_content('I am a Python Craft expert!,sending this from my IDE')
-
-
-# with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
-#     smtp.ehlo()
-#     smtp.starttls()
-#     smtp.login('warlordwarlordwarlord007@gmail.com', 'nrtv iarr uwop zdwq')
-#     smtp.send_message(email)
-#     print('Job is done boss!!')
