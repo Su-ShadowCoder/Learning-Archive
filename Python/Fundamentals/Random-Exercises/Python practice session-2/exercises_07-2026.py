1641388dff9ab6304b92a15d912fd138c0d0f5a5
Sudarshen <sudarshenk@gmail.com>
2026-07-20 12:57:40 +0200

    refreshing python, making some exercises that allows me to warm up again.

--- Python/Fundamentals/Random-Exercises/Python practice session-2/exercises_07-2026.py
+++ Python/Fundamentals/Random-Exercises/Python practice session-2/exercises_07-2026.py
@@ -136,103 +136,211 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # \\\\\\\\\\\\\\\\\\\
-Exercise 6 — Grade Checker
+# Exercise 6 — Grade Checker
 
-Ask for a score (0–100).
+# Ask for a score (0–100).
 
-Print:
+# Print:
 
-A (90+)
-B (80–89)
-C (70–79)
-D (60–69)
-F (<60)
+# A (90+)
+# B (80–89)
+# C (70–79)
+# D (60–69)
+# F (<60)
 
-Use only if, elif, and else.
+# Use only if, elif, and else.
 
-Exercise 7 — Countdown
+# 1 ask for user input numbber in int. 2 validate the number and compare it within the parameter numbers. 3 use logic to assign the numebrs to score letters. 4 make the output print the correct score based on the assigned logic. 
 
-Using a while loop, print:
+# user_number_inp = int(input("Please enter the score to check its grade:\n"))
 
-5
-4
-3
-2
-1
-Lift off!
-Exercise 8 — Multiplication Table
+# if 90 <= user_number_inp:
+#     print("Grade: A")
+# elif 80 <= user_number_inp < 90:
+#     print("Grade: B")
+# elif 70 <= user_number_inp < 79:
+#     print("Grade: C")
+# elif 60 <= user_number_inp < 69:
+#     print("Grade: D")
+# else:
+#     print("Grade: F")
 
-Ask the user for a number.
 
-Print its multiplication table up to 10.
 
-Example:
+# Exercise 7 — Countdown
 
-7 x 1 = 7
-7 x 2 = 14
-...
-7 x 10 = 70
+# Using a while loop, print:
 
-Use a for loop.
+# 5
+# 4
+# 3
+# 2
+# 1
+# Lift off!
 
-Exercise 9 — Guess the Secret Number
+# Workflow chart: 1 use while loop. 2 first establish a stop count for the while loop outside of the loop. 3 when done with the countdown print lift off
 
-Set
 
-secret = 8
+# count = 1
+# c_down = 5
 
-Keep asking until the user guesses correctly.
+# while count <= 5:
+#     print(c_down)
+#     c_down -= 1
+#     count += 1
 
-Output:
+# print("Lift off!")
 
-Too high!
-Too low!
-Correct!
 
-Concepts:
+# Exercise 8 — Multiplication Table
 
-while
-if
-Exercise 10 — List Practice
+# Ask the user for a number.
 
-Create:
+# Print its multiplication table up to 10.
+
+# Example:
+
+# 7 x 1 = 7
+# 7 x 2 = 14
+# ...
+# 7 x 10 = 70
+
+# Use a for loop.
+
+
+# usr_inp = int(input("Please enter a number to present its multiplication table:\n"))
+
+# for numb in range( 1, 11):
+#     answer = usr_inp * numb 
+#     print(f"{usr_inp} x {numb} = {answer}")
+
+
+# Exercise 9 — Guess the Secret Number
+
+# Set
+
+# secret = 8
+
+# Keep asking until the user guesses correctly.
+
+# Output:
+
+# Too high!
+# Too low!
+# Correct!
+
+# Concepts:
+
+# while
+# if
+
+# secret = 8
+# state = True
+
+# while state:
+#     user_input = int(input("Please enter a number to guess the secret number:\n"))
+#     if user_input == secret:
+#         print("Correct!")
+#         state = False
+#     elif user_input > secret:
+#         print("Too high!")
+#     else:
+#         print("Too Low")
+
+
+
+# Exercise 10 — List Practice
+
+# Create:
 
 fruits = ["apple", "banana", "orange", "grape"]
 
-Do the following:
+# Do the following:
 
-Print the first item.
-Print the last item.
-Add "mango".
-Remove "banana".
-Print every fruit using a loop.
-Bonus Challenge — Mini ATM
+# Print the first item.
+# Print the last item.
+# Add "mango".
+# Remove "banana".
+# Print every fruit using a loop.
 
-Display:
 
-1. Check Balance
-2. Deposit
-3. Withdraw
-4. Exit
+# print(len(fruits))
+# print(fruits[0])
+# print(fruits[3])
 
-Start with:
+# fruits.append("mango")
+# fruits.remove("banana")
 
-balance = 1000
+# for element in fruits:
+#     print(element)
 
-The user chooses an option and the balance updates accordingly. You can ignore invalid input for now.
+
+
+# Bonus Challenge — Mini ATM
+
+# Display:
+
+# 1. Check Balance
+# 2. Deposit
+# 3. Withdraw
+# 4. Exit
+
+# Start with:
+
+# balance = 1000
+
+# The user chooses an option and the balance updates accordingly. You can ignore invalid input for now.
+
+
+# so the  atm must be working continously. and should return after what it has done to the main menu. 
+#  first thing first is oop. make a class called ATM
+#  make a sub class for balance 
+# make classmethods for 1, 2, 3 and used that trough poly morpism on the subclass
+# Exit will exit the loop and stop working the atm. 
+
+# okey nevermind i might have to cut corners as i forgot to like that you have do more thing when doing poly morpism like adding stuff to the main init class from ths subclass and stuff
+
+
+class ATM:
+
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+    def check_balance(self):
+        print(f"Your balance is: {self.balance}")
+
+    def deposit(self):
+        usr_input = int(input("Please enter how much you want to deposit:\n"))
+        self.balance += usr_input
+        print(f"New balance: {self.balance}")
+
+    def withdraw(self):
+        usr_input = int(input("Please enter the amount you would like to withdraw:\n"))
+        if usr_input > self.balance:
+            print(f"Invalid overdraft!, you have insufficient funds!\nBalance: {self.balance}")
+        else:
+            self.balance -= usr_input
+            print(f"The amount requested has been drawn.\nBalance: {self.balance}")
+    
+
+ATM1 = ATM("ATM1", 1000)
+
+Active = True 
+DeActivate = False
+status = Active
+
+while status:
+    user_input = int(input("Please select the following options:\n1. Check Balance\n2. Deposit\n3. Withdraw\n4. Exit\n"))
+    if user_input == 4:
+        status = DeActivate
+    elif user_input == 1:
+        ATM1.check_balance()
+    elif user_input == 2:
+        ATM1.deposit()
+    elif user_input == 3:
+        ATM1.withdraw()
+
+# daaaanggggg did i just crafted a virtual atm machine??? dangngggg. Alhamdulillah!!!
+
