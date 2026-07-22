b258754dafa8d0203516c21f67c1057eb277e744
Sudarshen <sudarshenk@gmail.com>
2026-07-22 16:57:47 +0200

    refreshing function and returning it

--- Python/Fundamentals/Random-Exercises/Python practice session-3/functions.py
+++ Python/Fundamentals/Random-Exercises/Python practice session-3/functions.py
@@ -46,3 +46,182 @@ def name_announcement_x5(name="Stranger!"):
 # box_of_stars()
 
 # name_anouncement_x5()
+
+
+#######################################################
+# Next Topic: Return Values (return) ⭐⭐⭐⭐⭐
+#######################################################
+
+
+# Exercises
+
+# Don't skip these.
+
+# 1. Add two numbers
+
+# Make a function that returns the sum.
+
+
+def add(x, y):
+    return x + y
+
+# result = add(10, 15)
+
+# print(result)
+
+
+
+# 2. Multiply two numbers
+
+# Return the product.
+
+
+
+def multiply(x, y):
+    return x * y
+
+# result = multiply(5, 4)
+
+# print(result)
+
+
+
+# 3. Find the largest number
+
+# Example:
+
+# largest = max_number(10, 20)
+
+# print(largest)
+
+# Output:
+
+# 20
+
+
+
+def largest_numb(x, y):
+    if x > y:
+        return x
+    elif x < y:
+        return y
+    else:
+        return f"{x} and {y} are equel"
+
+# result = largest_numb(2, 8)
+
+# print(result)
+
+
+
+
+# 4. Is Even?
+
+# Return True or False.
+
+# Example:
+
+# print(is_even(10))
+
+# Output:
+
+# True
+
+def is_even(x):
+    return x % 2 == 0
+
+result = is_even(17)
+
+print(result)
+
+
+# 5. Grade Checker
+
+# Input:
+
+# 95
+
+# Return:
+
+# "A"
+
+# Don't print inside the function.
+
+# Return it.
+
+
+def grade_check(x):
+    if x >= 90:
+        return "A"
+    elif x >= 80:
+        return "B"
+    elif x >= 70:
+        return "C"
+    elif x >= 60:
+        return "D"
+    else:
+        return "F"
+
+# result = grade_check(78)
+
+# print(result)
+
+
+
+
+# 6. BMI Calculator
+
+# Input:
+
+# height // weight
+
+# Return:
+
+# BMI
+
+
+def bmi_stat(weight, height):
+    return weight / (height ** 2)
+
+result = bmi_stat(140, 1.69)
+
+print(result)
+
+
+# 7. Rectangle Area
+
+# Input:
+
+# width
+# height
+
+# Return:
+
+# area
+
+
+
+def rect_area(width, height):
+    return width * height
+
+# result = rect_area(8, 8)
+
+# print(result)
+
+
+
+
+# 8. Temperature Converter
+
+# Celsius → Fahrenheit
+
+# Return the converted temperature.
+
+
+def tempcc_to_f(c):
+    return round(c * (9 / 5) + 32, 2)
+
+# result = tempcc_to_f(26)
+
+# print(result)
+
