f2cbd4ffc607e306ae9e1706167aa2faf0267121
Sudarshen <sudarshenk@gmail.com>
2026-07-23 11:03:06 +0200

    refreshing dictionaries content

---
+++ Python/Fundamentals/Random-Exercises/Python practice session-3/dictionaries.py
@@ -0,0 +1,155 @@
+# 🟡 Phase 2 — Dictionaries ⭐⭐⭐⭐⭐
+
+# Exercises
+
+# No scrolling! 😄
+
+
+
+# Exercise 1
+
+# Create a dictionary named book.
+
+# Store:
+
+# title
+# author
+# pages
+
+# Print the entire dictionary.
+
+
+book = {
+    'title': 'Al het blauw van de hemel',
+    'author': 'Melissa Da Costa',
+    'pages': 640
+}
+
+
+# print(book)
+
+
+
+# Exercise 2
+
+# Create a dictionary called movie.
+
+# Store:
+
+# title
+# year
+# rating
+
+# Print only the title.
+
+movie = {
+    'title': 'Matrix',
+    'year': 1999,
+    'rating': 83
+}
+
+# print(movie['title'])
+
+
+
+
+
+
+# Exercise 3
+
+# Create a dictionary called computer.
+
+# Store:
+
+# CPU
+# RAM
+# GPU
+
+# Print the GPU.
+
+
+computer = {
+    'CPU': 'AMD747',
+    'RAM': '16G - ADRX',
+    'GPU': 'NVIDEA 1080 RI'
+}
+
+# print(computer['GPU'])
+
+
+
+# Exercise 4
+
+# Create a dictionary called student.
+
+# Store:
+
+# name
+# age
+# course
+# passed (True or False)
+
+# Print:
+
+# name
+# course
+
+
+student = {
+    'name': 'Mike',
+    'age': 23,
+    'course': 'physics',
+    'passed': True
+}
+
+# print(student['name'])
+# print(student['course'])
+
+
+
+
+
+# Exercise 5
+
+# Create your own dictionary.
+
+# Minimum:
+
+# 5 keys
+# Mix at least three different data types (for example: string, integer, boolean)
+
+# Print each value individually.
+
+
+
+character = {
+    'name': 'Swordsman',
+    'Attack-power': 20,
+    'Online': True,
+    'Alive': False,
+    'Buff': "DOT per attack"
+}
+
+
+
+
+
+# Mini Challenge ⭐⭐⭐
+
+# Create a dictionary describing your cybersecurity journey.
+
+# Something like:
+
+journey = {
+    "goal": "CyberSecurity Engineer",
+    "current_topic": "Dictionaries",
+    "python_completed": "71%",
+    "linux": "Ubuntu",
+    "htb": 'InfoSec Introduction'
+}
+
+# Choose your own keys and values.
+
+
+print(journey['goal'])
+
