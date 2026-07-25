4a82279fb4b1b3e23d87d1970d101e17a6e31d33
Sudarshen <sudarshenk@gmail.com>
2026-07-25 03:03:53 +0200

    accessing information:key, values and item from dicts

---
+++ Python/Fundamentals/Random-Exercises/Python practice session-3/dict_mod2.py
@@ -0,0 +1,175 @@
+# Exercise 1
+
+# Create
+
+book = {
+    "title": "...",
+    "author": "...",
+    "pages": ...
+}
+
+# Print:
+
+# all keys
+
+
+# print(book.keys())
+
+
+
+
+# Exercise 2
+
+# Using the same dictionary,
+
+# print
+
+# all values
+
+
+# print(book.values())
+
+
+
+# Exercise 3
+
+# Using the same dictionary,
+
+# print
+
+# all items
+
+# print(book.items())
+
+
+
+
+# Exercise 4
+
+# Create
+
+server = {
+    "hostname": "Server-01",
+    "ip": "10.0.0.5",
+    "status": "Online",
+    "users": 18
+}
+
+# Print:
+
+# keys
+# values
+# items
+
+# print(server.keys())
+# print(server.values())
+# print(server.items())
+
+
+# Exercise 5
+
+# Create
+
+phone = {
+    "brand": "...",
+    "model": "...",
+    "storage": ...,
+    "battery": ...
+}
+
+# Print:
+
+# .keys()
+# .values()
+# .items()
+
+# print(phone.keys())
+# print(phone.values())
+# print(phone.items())
+
+
+
+# at this  point i am just copy and pasting to be honest
+
+
+
+
+
+# ⭐ Cyber Exercise
+
+# Create
+
+# event = {
+
+# with
+
+# username
+# ip_address
+# event_type
+# timestamp
+# severity
+
+# Now print
+
+# event.keys()
+
+# Then
+
+# event.values()
+
+# Then
+
+# event.items()
+
+
+
+event = {
+    'username': 'water123',
+    'ip_address': '192.168.24.1',
+    'event_type': 'login_attempt',
+    'timestamp': '15:24',
+    'severity': 'High'
+}
+
+# print(event.keys())
+# print(event.values())
+# print(event.items())
+ 
+
+
+# 🎯 Challenge
+
+# Create a dictionary describing your PC.
+
+# Include at least 7 keys.
+
+# For example:
+
+# CPU
+# GPU
+# RAM
+# SSD
+# OS
+# Python version
+# Hostname
+
+# Then print:
+
+# pc.keys()
+# pc.values()
+# pc.items()
+
+pc = {
+    'CPU': 'AMD',
+    'GPU': 'NVIDEA 5010',
+    'RAM': '32G ALTEC',
+    'SSD': '250G SAMSUNG',
+    'OS': 'Windows 15',
+    'Python_ver': 3.2,
+    'Hostname': 'sicko'
+}
+
+print(pc.keys())
+print(pc.values())
+print(pc.items())
+
