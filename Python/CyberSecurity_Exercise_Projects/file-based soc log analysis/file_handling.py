98c88a9c5290f91132394928054fcb39bfbb454b
Sudarshen <sudarshenk@gmail.com>
2026-08-13 20:22:13 +0200

    Today did gpt exercises where i had to code failed event extractor that extracted failed events from a log file and put it in list of dict that could be re-used

--- Python/CyberSecurity_Exercise_Projects/file-based soc log analysis/file_handling.py
+++ Python/CyberSecurity_Exercise_Projects/file-based soc log analysis/file_handling.py
@@ -53,18 +53,65 @@
 
 # 5. 
 
-def count_failed_users(file):
-    dict_failed_users = {}
-    with open(file) as f:
-        for line in f:
-            if "FAILED" in line:
-                key = line.split("user=")[1].strip("\n")
-                if key not in dict_failed_users:
-                    dict_failed_users[key] = 1
-                else:
-                    dict_failed_users[key] += 1
-    return dict_failed_users
+# def count_failed_users(file):
+#     dict_failed_users = {}
+#     with open(file) as f:
+#         for line in f:
+#             if "FAILED" in line:
+#                 key = line.split("user=")[1].strip("\n")
+#                 if key not in dict_failed_users:
+#                     dict_failed_users[key] = 1
+#                 else:
+#                     dict_failed_users[key] += 1
+#     return dict_failed_users
 
 
 
-print(count_failed_users("login_logs.txt"))
+# print(count_failed_users("login_logs.txt"))
+
+# 6. 
+
+
+# def count_failed_users(file):
+#     temp_dict = {}
+#     with open(file)  as f:
+#         for line in f:
+#             if "FAILED user=" in line:
+#                 key = line.split("user=")[1].strip()
+#                 if key not in temp_dict:
+#                     temp_dict[key] = 1
+#                 else:
+#                     temp_dict[key] += 1
+#     return temp_dict
+
+# print(count_failed_users("logs2.txt"))
+
+
+# # 7. 
+
+
+# # extract failed avents from selected or entered file
+
+# def extract_failed_events(file):
+#     extracted_failed_events = []
+#     with open(file) as f:
+#         for event in f:
+#             if "FAILED LOGIN" in event:
+#                 splitted_line = event.split("user=")[1]
+#                 value_user = splitted_line.split()[0]
+#                 value_ip = event.split("ip=")[1].strip()
+#                 this_dict = {"username": value_user, "ip": value_ip}
+#                 extracted_failed_events.append(this_dict)
+#     return extracted_failed_events
+
+# print(extract_failed_events("logs3.txt"))
+
+
+# # Expected return value
+# # [
+# #     {"username": "shadow", "ip": "192.168.1.20"},
+# #     {"username": "guest", "ip": "172.16.5.10"},
+# #     {"username": "shadow", "ip": "192.168.1.20"}
+# # ]
+
+
