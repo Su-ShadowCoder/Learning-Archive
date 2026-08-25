312a1d2e44392c56428a156063cd7d511614dccc
Sudarshen <sudarshenk@gmail.com>
2026-08-25 18:00:29 +0200

    Following the ZTM course to make a server, and learn how to set up a server trough python. today i set up a local server, added html files to the templates and explored what i could do and followed the course objectives.

---
+++ Python/Web-Dev_with_python/ZTM_Portfolio_Project/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+print(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
+@app.route("/blog")
+def blog():
+    return "<p>These are my thoughts on blogs</p>"
+
+@app.route("/blog/2020/cats")
+def blog2():
+    return "<p>I like cats</p>"
+
