7c0a658fa09067117da2dc661ed170298f96d675
Sudarshen <sudarshenk@gmail.com>
2026-09-13 02:26:30 +0200

    setting up a database, and connecting the database file with the back-end python code

--- Python/Web-Dev_with_python/ZTM_Portfolio_Project/server.py
+++ Python/Web-Dev_with_python/ZTM_Portfolio_Project/server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
+import os
+import csv
 
 app = Flask(__name__)
 print(__name__)
@@ -16,14 +18,36 @@ def html_page(page_name):
 
 
 
+def write_to_file(data):
+    with open("database.txt", 'a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email},{subject},{message}')
+
+
+def write_to_csv(data):
+    with open("database.csv", 'a', newline='', encoding='utf-8') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+
+
+
 #submit_form
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == "POST":
-        data = request.form.to_dict()
-        print(data)
-        return redirect('/thankyou.html/')
+        try:
+            data1 = request.form.to_dict()
+            write_to_csv(data1)
+            return redirect('/thankyou.html/')
+        except:
+            return 'did not save to database'
     else:
         return "something went wrong. Try again!"
+
 
 
